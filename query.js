0fbaf81099469d6a8fff757f607f3727f4006e5c
Wesley <7wesleymiller@gmail.com>
2021-11-08 21:37:08 -0500

    Chart now accepts data via ejs variable, sample query defined

---
+++ query.js
@@ -0,0 +1,62 @@
+// Import the Google Cloud client library using default credentials
+const { BigQuery } = require("@google-cloud/bigquery");
+const bigquery = new BigQuery();
+
+module.exports = {
+  //BigQuery data
+  query: async function() {
+    // Queries the U.S. given names dataset for the state of Texas.
+
+    const query = `SELECT name
+      FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
+      WHERE state = 'TX'
+      LIMIT 100`;
+
+    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+    const options = {
+      query: query,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: "US",
+    };
+
+    // Run the query as a job
+    const [job] = await bigquery.createQueryJob(options);
+    console.log(`Job ${job.id} started.`);
+
+    // Wait for the query to finish
+    const [rows] = await job.getQueryResults();
+
+    // Print the results
+    console.log("Rows:");
+    rows.forEach((row) => console.log(row));
+  },
+  //Default data to ensure everything is working
+  defaultQuery: function() {
+    return {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+  },
+};
