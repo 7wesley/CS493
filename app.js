107ab1b390dff01c550e571cd5f7cf596087145b
Wesley <7wesleymiller@gmail.com>
2021-11-08 20:54:55 -0500

    Add sample chart

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || "5000";
 
-/** Routes */
-
 //GET
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
