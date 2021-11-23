5f97c1b3b3aea022c501a74d2220b5f4118b6fb3
milobyte <aMilesStanley@gmail.com>
2021-11-23 17:44:05 -0500

    App.js can now graph COVID statistics by US State

--- query.js
+++ query.js
@@ -1,16 +1,12 @@
 // Import the Google Cloud client library using default credentials
-const { BigQuery } = require("@google-cloud/bigquery");
+const {
+  BigQuery
+} = require("@google-cloud/bigquery");
 const bigquery = new BigQuery();
 
 module.exports = {
   //BigQuery data
-  query: async function (q) {
-    // Queries the U.S. given names dataset for the state of Texas.
-
-    // const query = `SELECT name
-    //   FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
-    //   WHERE state = 'TX'
-    //   LIMIT 100`;
+  query: async function(q) {
 
     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
     const options = {
@@ -26,39 +22,66 @@ module.exports = {
     // Wait for the query to finish
     const [rows] = await job.getQueryResults();
 
-    // Print the results
-    // console.log("Rows:");
-    // rows.forEach((row) => console.log(row));
-
     return rows;
   },
+
+  cases_by_state: async function(state_code) {
+    //Query used to gather COVID related data
+    const state_query = "SELECT date, confirmed_cases FROM bigquery-public-data.covid19_govt_response.oxford_policy_tracker\n" +
+      "WHERE region_code = '" + state_code + "' AND confirmed_cases IS NOT NULL\n" +
+      "ORDER BY date ASC\n" +
+      "LIMIT 1000;"
+
+    //Getting data by connecting with GCP for state data
+    let data = await module.exports.query(state_query);
+
+    //Configuring data into an object that can be graphed using chart.js
+    let dates = [];
+    let cases = [];
+    data.forEach((row) => {
+      dates.push(row.date.value);
+      cases.push(row.confirmed_cases);
+    });
+    const line_data = {
+      labels: dates,
+      datasets: [{
+        label: state_code + ' Covid Cases',
+        data: cases,
+      }]
+    };
+
+    //returning object needed to render the line graph
+    return {
+      data: line_data,
+      type: 'line'
+    };
+  },
+
   //Default data to ensure everything is working
-  defaultQuery: function () {
+  defaultQuery: function() {
     return {
       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
+      datasets: [{
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      }, ],
     };
   },
 };
