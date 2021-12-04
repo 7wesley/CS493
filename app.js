3429f98757f3447d02a281bfc1ed3d2178136835
milobyte <aMilesStanley@gmail.com>
2021-12-04 09:08:25 -0500

    Added homepage

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ const port = process.env.PORT || "5000";
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+//Parses body of html files for data
+app.use(express.urlencoded({
+  extended: true
+}));
+
 //GET
 app.get("/", (req, res) => {
-  let state_graph = bigQuery.cases_by_state('US_NC');
-  state_graph.then((graph_data) => res.render("index", {graph_data: graph_data}));
+  res.render("home");
 });
 
-app.get("/record2", (req, res) => {
-  let vaccine_graph = bigQuery.vaccination_hospitalization('US_NC')
-  vaccine_graph.then((graph_data) => res.render("index", {graph_data: graph_data}));
-});
-
-app.get("/record3", (req, res) => {
-  let percentage_graph = bigQuery.percentage_comparison('US_NC')
-  percentage_graph.then((graph_data) => res.render("index", {graph_data: graph_data}));
-});
+app.post("/graph_visualization", (req, res) => {
+  let state_code = req.body.state_select;
+  let graph_choice = req.body.graph_type;
+  if (graph_choice === 'percentage_results') {
+    let percentage_graph = bigQuery.percentage_comparison(state_code)
+    percentage_graph.then((graph_data) => res.render("index", {graph_data: graph_data}));
+  }
+  else if (graph_choice === 'cumulative_results'){
+    let vaccine_graph = bigQuery.vaccination_hospitalization(state_code)
+    vaccine_graph.then((graph_data) => res.render("index", {graph_data: graph_data}));
+  }
+  else {
+    let state_graph = bigQuery.cases_by_state(state_code);
+    state_graph.then((graph_data) => res.render("index", {graph_data: graph_data}));
+  }
+})
 
 /** Listen for requests @ port */
 app.listen(port, () => {
